fc3a537cd1046db9f2bdb06a4492d23c567a2286
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-06-01 10:38:07 +0200

    changes in task-4

--- js/task-4.js
+++ js/task-4.js
@@ -11,15 +11,15 @@ formElement.addEventListener("submit", (event) => {
   const emailValue = formElement.elements.email.value.trim();
   const passwordValue = formElement.elements.password.value.trim();
 
-  if (emailValue === "" || passwordValue === "") {
+  if (emailValue !== "" && passwordValue !== "") {
+    const formData = new FormData(formElement);
+    const data = {
+      email: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(data);
+    formElement.reset();
+  } else {
     alert("All form fields must be filled in");
   }
-
-  const formData = new FormData(formElement);
-  const data = {
-    email: formData.get("email"),
-    password: formData.get("password"),
-  };
-  console.log(data);
-  formElement.reset();
 });
